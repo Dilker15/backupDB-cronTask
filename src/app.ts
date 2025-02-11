7d68de50ed8a62a550338ba6538e0ee304bbef17
Dilker15 <dilker72@gmail.com>
2025-02-11 14:59:04 -0400

    Sql server strategy

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv'
 dotenv.config();
-import { PostgresStrategy } from "./infraestructure/backups/PostgresStrategy";
-
 
 import { Backup } from "./domain/usecases/Backup";
+import { SqlServerStrategy } from './infraestructure/backups/SqlServerStrategy';
+import { PostgresStrategy } from './infraestructure/backups/PostgresStrategy';
+
+
 
 const contextBackup = new Backup(new PostgresStrategy());
 contextBackup.execute('postgres','postgres123','shorten_url','localhost');
+
+
+
+contextBackup.setStrategy(new SqlServerStrategy());
+contextBackup.execute('BNTB125\dilker', '', 'supermarket', 'localhost');
 
